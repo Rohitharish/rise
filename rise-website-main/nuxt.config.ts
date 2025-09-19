3ef73ec4e84a61f3d4877c0cb6d9d0b6bb839bea
Rohit Harish <rohitharish276@gmail.com>
2025-09-19 05:52:26 -1200

    add deploy fix

--- rise-website-main/nuxt.config.ts
+++ rise-website-main/nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ["~/assets/styles/main.scss"]
+  css: ["~/assets/styles/main.scss"],
+  nitro: {
+    prerender: {
+      routes: ['/invoices/austral-care', '/invoices/austral-care-alt', '/invoices/edenvale-farm']
+    }
+  },
+  app: {
+    baseURL: '/'
+  }
 })
